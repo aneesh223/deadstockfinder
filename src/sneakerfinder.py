7a6a09bea89c93196618c1116fceb0c9474dabf8
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2023-06-21 11:06:01 -0400

    Update Dependencies

--- src/sneakerfinder.py
+++ src/sneakerfinder.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import requests
-import json
 
 
 class SneakerFinder:
